5874a2359f9e54933ec2f2238d00b78a30c3d278
Muhamad Rio Maulana Akbar <rio605051@gmail.com>
2026-03-16 06:24:44 +0700

    add soal1 modul3

--- Modul 3/laporan_praktikum.md
+++ Modul 3/laporan_praktikum.md
@@ -1,5 +1,5 @@
 # <h1 align="center">Laporan Praktikum Modul 1 - ... </h1>
-<p align="center">[nama] - [NIM]</p>
+<p align="center">[MUHAMAD RIO MAULANA AKBAR] - [109082500187]</p>
 
 ## Unguided 
 
--- Modul 3/soal1.go
+++ Modul 3/soal1.go
@@ -1,5 +1,34 @@
 package main
 import "fmt"
+func faktorial(n int)int{
+	var hasil int = 1
+	var i int
+	for i = 1; i <= n; i++{
+		hasil = hasil * i
+	}
+	return hasil
+}
+func permutasi(n, r int)int{
+	return faktorial(n) / faktorial(n-r)
+}
+func kombinasi(n, r int)int {
+	return permutasi(n,r) / faktorial(r)
+}
 func main() {
-	
+	var a, b, c, d int
+
+	fmt.Scan(&a, &b, &c, &d)
+	if a >= c && b >= d {
+		fmt.Printf("%d, %d\n", permutasi(a,c), kombinasi(a,c))
+		fmt.Printf("%d, %d\n", permutasi(b,d), kombinasi(b,d))
+	} else if c >= a && b>= d{
+		fmt.Printf("%d, %d\n", permutasi(c,a), kombinasi(c,a))
+		fmt.Printf("%d, %d\n", permutasi(b,d), kombinasi(b,d))
+	}else if a >= c && d >= b {
+		fmt.Printf("%d, %d\n", permutasi(a,c), kombinasi(a,c))
+		fmt.Printf("%d, %d\n", permutasi(d,b), kombinasi(d,b))
+	}else if c >= a && d>= b{
+		fmt.Printf("%d, %d\n", permutasi(c,a), kombinasi(c,a))
+		fmt.Printf("%d, %d\n", permutasi(d,b), kombinasi(d,b))
+	}
 }
